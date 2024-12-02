8160bb81b79d8e1eafafd3977cfe564096bf0224
Millun Atluri <millun.atluri@gmail.com>
2024-12-02 14:38:02 -0500

    Day 1 Pt 1

---
+++ day1/day1.py
@@ -0,0 +1,19 @@
+file_path = 'pt1_input.txt'
+
+distances = []
+left_list  = []
+right_list = []
+
+with open(file_path, 'r') as file:
+    for line in file:
+        row = line.strip().split("   ")
+        left_list.append(row[0])
+        right_list.append(row[1])
+        
+for i in range(len(left_list)):
+    left = left_list[i]
+    right = right_list[i]
+    distance = abs(int(left) - int(right))
+    distances.append(distance)
+
+print(sum(distances))
