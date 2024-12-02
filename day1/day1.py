27fe2f771bbfbb2909f6f069eb1bf571d814e7fa
Millun Atluri <millun.atluri@gmail.com>
2024-12-02 14:46:53 -0500

    format updates

--- day1/day1.py
+++ day1/day1.py
@@ -1,22 +1,27 @@
 file_path = 'pt1_input.txt'
 
-distances = []
-left_list  = []
-right_list = []
+def day1pt1():
+    
+    distances = []
+    left_list  = []
+    right_list = []
 
-with open(file_path, 'r') as file:
-    for line in file:
-        row = line.strip().split("   ")
-        left_list.append(row[0])
-        right_list.append(row[1])
-        
-left_list.sort()
-right_list.sort()
+    with open(file_path, 'r') as file:
+        for line in file:
+            row = line.strip().split("   ")
+            left_list.append(row[0])
+            right_list.append(row[1])
+            
+    left_list.sort()
+    right_list.sort()
 
-for i in range(len(left_list)):
-    left = left_list[i]
-    right = right_list[i]
-    distance = abs(int(left) - int(right))
-    distances.append(distance)
+    for i in range(len(left_list)):
+        left = left_list[i]
+        right = right_list[i]
+        distance = abs(int(left) - int(right))
+        distances.append(distance)
 
-print(sum(distances))
+    return sum(distances)
+
+def main():
+    print(day1pt1())
