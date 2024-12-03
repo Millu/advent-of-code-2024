a53bc4eabffced857a5391cabbffb384fa36a65e
Millun Atluri <millun.atluri@gmail.com>
2024-12-03 18:59:36 -0500

    Day 3 Pt 1 First attempt

---
+++ day3/day3.py
@@ -0,0 +1,29 @@
+import regex as re
+
+
+def day3_pt1(file):
+
+    sum = 0
+    regex_to_find_multiples = 'mul\(\d{1,3},\d{1,3}\)'
+    matches = []
+    with open (file, 'r') as file:
+        for line in file:
+            matches.extend(re.findall(regex_to_find_multiples, line))
+                    
+    
+    print(matches)
+    print(len(matches))
+
+     
+    for i in matches:
+        numbers = re.findall('([1-9]*)', i)
+        numbers[:] = [x for x in numbers if x]
+        print(numbers)
+        sum += int(numbers[0]) * int(numbers[1])
+        
+    print('Sum:', sum)
+        
+
+if __name__ == "__main__":
+    file_name = 'day3_example.txt'
+    day3_pt1(file_name)
