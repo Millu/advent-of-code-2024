fa27fbe3969e3cdab5589fcb805d408a4c1ea183
Millun Atluri <millun.atluri@gmail.com>
2024-12-03 15:18:54 -0500

    day 2 close.. but not quite

--- day2/day2.py
+++ day2/day2.py
@@ -44,8 +44,6 @@ def day2_pt2(file_path):
     # Delete the number from the list, add 1 to the unsafe level counter and check the list again
     # If the level is found unsafe and the unsafe level counter is > 1 then the level is unsafe
 
-
-
     total_safe_levels = 0
 
     with open(file_path, 'r') as file:
@@ -55,42 +53,65 @@ def day2_pt2(file_path):
             direction = ''
             is_level_safe = False
             num_unsafe_levels = 0
-
+            
             if int(level[1]) > int(level[0]):
                 direction = 'increasing'
             else:
                 direction = 'decreasing'
             
-            for i in range(1,len(level)):
-                
+            checked_levels = level.copy()
+            i = 1
+            print('level: ', level)
+            while i < len(checked_levels):
+                print('level_being_checked:', checked_levels)
                 diff = abs(int(level[i]) - int(level[i-1]))
                 
                 if diff >= 1 and diff <= 3:
-                    if i > 1 and direction == 'increasing' and int(level[i]) > int(level[i-1]):
-                        print('increasing')
+                    if i > 1 and direction == 'increasing' and int(checked_levels[i]) > int(checked_levels[i-1]):
                         is_level_safe = True
-                    elif i > 1 and direction == 'decreasing' and int(level[i]) < int(level[i-1]):
-                        print('descreasing')
+                    elif i > 1 and direction == 'decreasing' and int(checked_levels[i]) < int(checked_levels[i-1]):
                         is_level_safe = True
                     elif i == 1:
-                         pass
+                        pass
                     else:
-                        print('diff:', diff, 'i:', level[i], 'i-1:', level[i-1]) 
-                        num_unsafe_levels += 1
-                        if num_unsafe_levels > 1:
-                            print('unsafe levels in count:', num_unsafe_levels, level,)
+                        if num_unsafe_levels == 0:
+                            checked_levels.pop(i)
+                            print('0000   ','i:', i, 'level:', checked_levels)
+                            num_unsafe_levels += 1
+                        elif num_unsafe_levels == 1:
+                            checked_levels = level.copy()
+                            checked_levels.pop(i)
+                            print('1111   ','i:', i, 'level:', checked_levels)
+                            num_unsafe_levels += 1
+                        elif num_unsafe_levels == 2:
+                            print('2222   ','i:', i, 'level:', checked_levels)
                             is_level_safe = False
+                            print('Unsafe level:', level, '\n')
                             break
+                        
+                        i = 1
+
                 else:                   
-                    num_unsafe_levels += 1
-                    print('unsafe levels:', num_unsafe_levels, level, )
-                    if num_unsafe_levels > 1:
-                        print('break')
+                    if num_unsafe_levels == 0:
+                            checked_levels.pop(i)
+                            print('0000   ','i:', i, 'level:', checked_levels)
+                            num_unsafe_levels += 1
+                    elif num_unsafe_levels == 1:
+                        checked_levels = level.copy()
+                        checked_levels.pop(i)
+                        print('1111   ','i:', i, 'level:', checked_levels)
+                        num_unsafe_levels += 1
+                    elif num_unsafe_levels == 2:
+                        print('2222   ','i:', i, 'level:', checked_levels)
                         is_level_safe = False
+                        print('Unsafe level:', level, '\n')
                         break
+                    
+                    i = 1
+
+                i += 1
             
             if is_level_safe:
-                print('level is safe:', is_level_safe, level, '\n')
                 total_safe_levels += 1
 
     print("Safe levels including 1 bad level: ", total_safe_levels)
@@ -99,6 +120,7 @@ def day2_pt2(file_path):
 if __name__ == "__main__":
     
     file_path = 'day2_example.txt'
+    print(file_path, '\n')
     day2_pt1(file_path)
     day2_pt2(file_path)
 
