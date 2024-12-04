d4856f2be944f95b71efc4779618b0b06564fff5
Millun Atluri <millun.atluri@gmail.com>
2024-12-03 19:19:35 -0500

    day 3 pt1

--- day3/day3.py
+++ day3/day3.py
@@ -9,21 +9,16 @@ def day3_pt1(file):
     with open (file, 'r') as file:
         for line in file:
             matches.extend(re.findall(regex_to_find_multiples, line))
-                    
-    
-    print(matches)
-    print(len(matches))
 
-     
     for i in matches:
-        numbers = re.findall('([1-9]*)', i)
+        numbers = re.findall('([0-9]*)', i)
         numbers[:] = [x for x in numbers if x]
         print(numbers)
         sum += int(numbers[0]) * int(numbers[1])
         
-    print('Sum:', sum)
+    print('Sum:', sum)        
         
 
 if __name__ == "__main__":
-    file_name = 'day3_example.txt'
+    file_name = 'day3.txt'
     day3_pt1(file_name)
